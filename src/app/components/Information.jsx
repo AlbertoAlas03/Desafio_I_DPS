4e819bbe57ef2cad0c513989e5a343d2a9a4c14d
Alberto Alas <oa3973392@gmail.com>
2024-07-27 10:35:26 -0600

    Cambio de diseño CSS

--- src/app/components/Information.jsx
+++ src/app/components/Information.jsx
@@ -1,29 +1,46 @@
-import react from "react";
+import React from "react";
 import { data } from "./data";
 import { Title } from "./Title";
 
 export const Informacion = () => {
-    return (
-        <div className="container marketing">
-            <Title />
-            {data[0].empresas.map((empresa) => ( //iteración de data, ingresamos al primer elemento del array
-                <div className="row featurette" key={empresa.id}>
-                    <div className="col-md-7">
-                        <h1 className="featurette-heading fw-normal lh-1"><b>{empresa.titulo}</b></h1><br />
-                        <h2 className="featurette-heading fw-normal lh-1">Descripción</h2><br />
-                        <p className="lead">{empresa.descripcion}</p><br />
-                        <h3 className="featurette-heading fw-normal lh-1">Caracteristicas</h3><br />
-                        <p className="lead">{empresa.caracteristicas}</p><br />
-                    </div>
-                    <div className="col-md-5">
-                        <img src={empresa.imagen} className="bd-placeholder-img bd-placeholder-img-lg featurette-image mx-auto" width="350" height="350" style={{ marginBottom: 20 }} />
-                    </div>
-                    <hr className="featurette-divider" />
-                </div>
-            ))}
-            <footer className="container">
-                <p>© Desarrollado por Oscar Alberto Alas Guzmán | Estudiante de Ingenieria en ciencias de la computación.</p>
-            </footer>
+  return (
+    <div className="container marketing my-5">
+      <Title />
+      {data[0].empresas.map((empresa) => ( //mapeo de data con el primer valor del array
+        <div className="row featurette mb-5" key={empresa.id}>
+          <div className="col-md-7 order-md-2 d-flex flex-column justify-content-center">
+            <h1 className="featurette-heading fw-bold lh-1 mb-3">
+              <b>{empresa.titulo}</b>
+            </h1>
+            <h2 className="featurette-subheading fw-light lh-1 mb-3" style={{color: "red"}}>
+              Descripción
+            </h2>
+            <p className="lead mb-4" style={{textAlign: "justify"}}>{empresa.descripcion}</p>
+            <h3 className="featurette-subheading fw-light lh-1 mb-3" style={{color: "blue"}}>
+              Características
+            </h3>
+            <p className="lead" style={{textAlign: "justify"}}>{empresa.caracteristicas}</p>
+          </div>
+          <div className="col-md-5 order-md-1">
+            <div className="image-container d-flex justify-content-center align-items-center">
+              <img
+                src={empresa.imagen}
+                alt={empresa.titulo}
+                className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto rounded"
+                width="350"
+                height="350"
+              />
+            </div>
+            <br />
+          </div>
+          <hr />
+
         </div>
-    );
-}
+      ))}
+      <hr />
+      <footer className="container text-center mt-5">
+        <p>© Desarrollado por Oscar Alberto Alas Guzmán | Estudiante de Ingeniería en Ciencias de la Computación.</p>
+      </footer>
+    </div>
+  );
+};
